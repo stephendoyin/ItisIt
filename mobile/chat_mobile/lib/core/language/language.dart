49fd8a5b8e33e9978b07f0ce077f73e69ee31cce
dong ha <van_dong9@yahoo.com>
2020-01-26 14:49:55 +0700

    update

--- mobile/chat_mobile/lib/core/language/language.dart
+++ mobile/chat_mobile/lib/core/language/language.dart
@@ -1,1 +1,11 @@
+abstract class Language {
+  String userName() => "";
+}
 
+class English implements Language {
+  String userName() => "";
+}
+
+class VietNamese implements Language {
+  String userName() => "";
+}
---
+++ mobile/chat_mobile/lib/models/user.dart
@@ -0,0 +1,7 @@
+class User {
+  String id;
+  String name;
+  String first_name;
+  String last_name;
+  String email;
+}
