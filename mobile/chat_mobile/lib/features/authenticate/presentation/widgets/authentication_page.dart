b8e29b2b5f5ac61f199b70da5e210fbd9c50f9aa
dong.ha <dong.ha@tpssoft.com>
2020-02-09 21:17:36 +0700

    add onboarding page

--- mobile/chat_mobile/lib/features/authenticate/presentation/widgets/authentication_page.dart
+++ mobile/chat_mobile/lib/features/authenticate/presentation/widgets/authentication_page.dart
@@ -9,99 +9,387 @@ class AuthenticationPage extends StatefulWidget {
 }
 
 class _AuthenticationPageState extends State<AuthenticationPage> {
-  final List<Widget> introWidgetsList = [
-    Container(child: Stack(children: <Widget>[
-      // Text("data"),Text("data"),Text("data"),Text("data")
-      // Container(
-      //   child: Image(image: AssetImage('assets/images/onboard/bg_gradient_01.png'),),
-      // ),
-      //       Container(
-      //   child: Image(image: AssetImage('assets/images/onboard/bg_gradient_01.png'),),
-      // ),
-
-      Image(image: AssetImage('assets/images/onboard/bg_gradient_01.png'),),
-      Image(image: AssetImage('assets/images/onboard/bg_gradient_02.png'),),
-      Image(image: AssetImage('assets/images/onboard/bg_gradient_03.png'),),
-      Image(image: AssetImage('assets/images/onboard/bg_gradient_04.png'),)
-
-    ],),)
+  final List<Widget> introWidgetsList = <Widget>[
+    Container(
+      child: Stack(
+        children: <Widget>[
+          Image(
+              image: AssetImage('assets/images/onboard/bg_gradient_01.png'),
+              fit: BoxFit.fitWidth),
+          SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Image.asset(
+                        'assets/images/onboard/ic_onboarding_send_receive.png',
+                        width: 300.0,
+                        height: 300.0),
+                    Text(
+                      "Send and receive payments to\nfriends and family quickly.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+    Container(
+      child: Stack(
+        children: <Widget>[
+          Image(
+              image: AssetImage('assets/images/onboard/bg_gradient_01.png'),
+              fit: BoxFit.fitWidth),
+          SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Image.asset('assets/images/onboard/ic_onboarding_pay.png',
+                        width: 300.0, height: 300.0),
+                    Text(
+                      "Quickly checkout at hundreds of\nmerchants throughout The Bahamas\nwith a single tap.",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+    Container(
+      child: Stack(
+        children: <Widget>[
+          Image(
+              image: AssetImage('assets/images/onboard/bg_gradient_03.png'),
+              fit: BoxFit.fitWidth),
+          SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Image.asset('assets/images/onboard/ic_onboarding_deals.png',
+                        width: 300.0, height: 300.0),
+                    Text(
+                      "When you spend you save. Our merchants offer exclusive deals daily. Take advantage and save.",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+    Container(
+      child: Stack(
+        children: <Widget>[
+          Image(
+              image: AssetImage('assets/images/onboard/bg_gradient_04.png'),
+              fit: BoxFit.fitWidth),
+          SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text("Prosper"),
+                    Text(
+                      "Out of the banks and in your hands.\nNo bank account or credit card required.\nLow to no transaction fees.",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    )
   ];
 
   int currentPageValue = 0;
   final controller = PageController(initialPage: 0);
 
-void getChangedPageAndMoveBar(int page) {
-  currentPageValue = page;
-  setState(() {});
-}
+  void getChangedPageAndMoveBar(int page) {
+    setState(() {
+      currentPageValue = page;
+    });
+  }
 
+  Widget pageForIndex(BuildContext context, int index) {
+    final screenSize = window.physicalSize; // MediaQuery.of(context).size;
+    switch (index) {
+      case 0:
+        return Container(
+          //  height: screenSize.height,
+          //  width: screenSize.width,
+          color: Colors.blue,
+          child: Stack(
+            children: <Widget>[
+              Image(
+                  image: AssetImage('assets/images/onboard/bg_gradient_01.png'),
+                  fit: BoxFit.cover,
+                  width: screenSize.width,
+                  height: screenSize.height),
+              // Image.asset('assets/images/onboard/bg_gradient_01.png', width: screenSize.width, height: screenSize.height),
+              SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Image.asset(
+                            'assets/images/onboard/ic_onboarding_send_receive.png',
+                            width: 300.0,
+                            height: 300.0),
+                        Text(
+                          "Send and receive payments to\nfriends and family quickly.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      case 1:
+        return Container(
+          //  height: screenSize.height,
+          //  width: screenSize.width,
+          color: Colors.blue,
+          child: Stack(
+            children: <Widget>[
+              Image(
+                  image: AssetImage('assets/images/onboard/bg_gradient_02.png'),
+                  fit: BoxFit.cover,
+                  width: screenSize.width,
+                  height: screenSize.height),
+              SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Image.asset(
+                            'assets/images/onboard/ic_onboarding_pay.png',
+                            width: 300.0,
+                            height: 300.0),
+                        Text(
+                          "Quickly checkout at hundreds of\nmerchants throughout The Bahamas\nwith a single tap.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      case 2:
+        return Container(
+          color: Colors.blue,
+          child: Stack(
+            children: <Widget>[
+              Image(
+                  image: AssetImage('assets/images/onboard/bg_gradient_03.png'),
+                  fit: BoxFit.cover,
+                  width: screenSize.width,
+                  height: screenSize.height),
+              SafeArea(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Image.asset('assets/images/onboard/ic_onboarding_deals.png',
+                        width: 300.0, height: 300.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 300,
+                          child: Text(
+                            "When you spend you save. Our merchants offer exclusive deals daily. Take advantage and save.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      case 3:
+        return Container(
+          color: Colors.blue,
+          child: Stack(
+            children: <Widget>[
+              Image(
+                  image: AssetImage('assets/images/onboard/bg_gradient_04.png'),
+                  fit: BoxFit.cover,
+                  width: screenSize.width,
+                  height: screenSize.height),
+              SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Container(
+                          width: 300.0,
+                          height: 300.0,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Prosper",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 70,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Flexible(
+                          child: Text(
+                            "Out of the banks and in your hands.\nNo bank account or credit card required.\nLow to no transaction fees.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+
+      default:
+        return Image(
+            image: AssetImage('assets/images/onboard/bg_gradient_02.png'),
+            fit: BoxFit.cover);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body: SafeArea(
-        child: Container(
-          child: Stack(
-            alignment: AlignmentDirectional.bottomCenter,
-            children: <Widget>[
-              // PageView.builder(physics: ClampingScrollPhysics(),
-              // itemCount: introWidgetsList.length,
-              // onPageChanged: (int page) {
-              //   getChangedPageAndMoveBar(page);
-              // },
-              // controller: controller,
-              // itemBuilder: (context, index) {
-              //   return introWidgetsList[index];
-              // }
-              // ),
-              PageView(
+      body: Container(
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: <Widget>[
+            PageView.builder(
                 physics: ClampingScrollPhysics(),
-                controller: controller,
+                itemCount: introWidgetsList.length,
                 onPageChanged: (int page) {
-                  setState(() {
-                    currentPageValue = page;
-                  });
+                  getChangedPageAndMoveBar(page);
                 },
-                children: introWidgetsList,
+                controller: controller,
+                itemBuilder: (context, index) {
+                  return pageForIndex(context, index);
+                }),
+            SafeArea(
+              child: Container(
+                child: Stack(
+                  alignment: AlignmentDirectional.topStart,
+                  children: <Widget>[
+                    Container(
+                        child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        for (int i = 0; i < introWidgetsList.length; i++)
+                          if (i == currentPageValue) ...[
+                            circleBar(true)
+                          ] else ...[
+                            circleBar(false)
+                          ]
+                      ],
+                    ))
+                  ],
+                ),
               ),
-              Stack(
-                alignment: AlignmentDirectional.topStart,
-                children: <Widget>[
-                  Container(child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      for (int i = 0; i < introWidgetsList.length; i++) 
-                        if (i == currentPageValue) ...[circleBar(true)] else ...[circleBar(false)]
-                      
-                    ],
-                  ))
-              ],),
-              Visibility(child: Align(
-                child: Container(child: FloatingActionButton(child: Icon(Icons.ac_unit),
-                onPressed: () {
-
-                },)),
+            ),
+            Visibility(
+              child: Align(
+                child: Container(
+                    child: FlatButton(
+                  child: Icon(Icons.ac_unit),
+                  onPressed: () {},
+                )),
               ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-
-// class PageView extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return null;
-//   }
-// }
-
-const Color kOrange = Colors.orange;
+const Color kOrange = Colors.white;
 const Color klightGrey = Colors.lightBlue;
 Widget circleBar(bool isActive) {
   return AnimatedContainer(
@@ -110,7 +398,8 @@ Widget circleBar(bool isActive) {
     height: isActive ? 12 : 8,
     width: isActive ? 12 : 8,
     decoration: BoxDecoration(
-        color: isActive ? kOrange : klightGrey,
+        color: isActive ? kOrange : Colors.transparent,
+        border: Border.all(width: 1, color: kOrange),
         borderRadius: BorderRadius.all(Radius.circular(12))),
   );
 }
--- mobile/chat_mobile/lib/features/splash_screen/splash_screen_page.dart
+++ mobile/chat_mobile/lib/features/splash_screen/splash_screen_page.dart
@@ -1,31 +1,76 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:chat_mobile/core/routes/navigation.dart';
 import 'package:chat_mobile/routers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController
+      _controller; // = AnimationController(duration: const Duration(milliseconds: 5000), vsync: this);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 6), vsync: this);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Timer.periodic(Duration(seconds: 5), (timer) {
+    Timer.periodic(Duration(seconds: 10), (timer) {
       timer.cancel();
       pushAuthenticationPage(context);
     });
     return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Text(
-            "Loading",
-            textAlign: TextAlign.center,
-          )
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [Color(0xff66328E), Color(0xffDE2055)])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Image.asset(
+                "assets/images/general/ic_app.png",
+                width: 281,
+                height: 76,
+              ),
+              Text(
+                "Powser to Prosper",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              RotationTransition(
+                turns:
+                    Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller),
+                child: Image.asset(
+                  'assets/images/general/ic_loading.png',
+                  width: 45.3,
+                  height: 45.3,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
